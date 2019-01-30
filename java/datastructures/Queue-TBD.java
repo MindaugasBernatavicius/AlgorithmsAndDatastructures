aa0aa98fb10af317573eaa912a4b76947760545a
MindaugasBernatavicius <darbas.mindaugas@gmail.com>
2019-01-30 09:38:27 +0200

    Update Queue-TBD.java

--- java/datastructures/Queue-TBD.java
+++ java/datastructures/Queue-TBD.java
@@ -1,1 +1,32 @@
+public class Queue {
+    public static void main(String args[]) {
+        QueueImpl q = new QueueImpl();
+    }
+}
 
+class QueueImpl {
+    
+    char[] queue;
+    int headIdx;
+    
+    public QueueImpl(){
+        queue = new char[5];
+    }
+    
+    public void enqueue(char c){
+        // add resizing logic
+        queue[headIdx] = c;
+        headIdx++;
+    }
+    public char dequeue(){
+        return queue[0];
+    }
+    
+    public char tail(){
+        return queue[0];
+    }
+    
+    public char head(){
+        return queue[headIdx];
+    }
+}
