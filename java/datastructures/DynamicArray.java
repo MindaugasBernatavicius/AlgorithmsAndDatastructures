45a8fb01c1982582d7d669835a646f3370d9f4bc
MindaugasBernatavicius <darbas.mindaugas@gmail.com>
2019-02-02 01:01:15 +0200

    Update DynamicArray.java

--- java/datastructures/DynamicArray.java
+++ java/datastructures/DynamicArray.java
@@ -1,12 +1,22 @@
 public class DynamicArray {
     public static void main(String args[]) {
         DynamicArrayImpl dArr = new DynamicArrayImpl(2);
-        dArr.add('a');
-        dArr.add('b');
-        System.out.print(dArr.getAt(1));
-        dArr.add('d');
-        dArr.add('e');
-        System.out.print(dArr.getAt(3));
+        // dArr.add('a');
+        // dArr.add('b');
+        // System.out.print(dArr.getAt(1));
+        // dArr.add('d');
+        // dArr.add('e');
+        // System.out.print(dArr.getAt(3));
+        
+        dArr.add(0, 'a');
+        dArr.add(1, 'b');
+        dArr.add(2, 'd');
+        dArr.add(3, 'e');
+        dArr.add(1, '1');
+        System.out.println(dArr.getAt(3));
+        
+        for(int i = 0; i < dArr.length(); i++)
+            System.out.print(dArr.getAt(i));
     }
 }
 
@@ -16,35 +26,51 @@ class DynamicArrayImpl {
     private int currentIdx;
     private char[] array;
     
-    DynamicArray(int initialSize){
+    public DynamicArrayImpl(int initialSize){
         this.initialSize = initialSize;
         array = new char[initialSize];
     }
 
     public void add(char c){
-        if(currentIdx >= array.length){
+        if(currentIdx == array.length){
             // create a new array
             char[] tmpArray = new char[array.length * 2];
-            
             // copy the old array and add new array
             for(int i = 0; i < array.length; i++)
                 tmpArray[i] = array[i];
-
             array = tmpArray;
             tmpArray = null;
         }
-        
         // add an element
         array[currentIdx++] = c;
     }
     
-    // public void addAt(int idx, char c){}
-    
-    public void remove(){
-        array[currentIdx] = '\0';
+    // an overloaded method
+    public void add(int idx, char c){
+        if(currentIdx == array.length){
+            char[] tmpArray = new char[array.length * 2];
+            for(int i = 0; i < idx; i++)
+                tmpArray[i] = array[i];
+            for(int i = idx + 1; i < array.length + 1; i++)
+                tmpArray[i] = array[i - 1];
+            tmpArray[idx] = c;
+            currentIdx++;
+            array = tmpArray;
+            tmpArray = null;
+        } else {
+            for(int i = currentIdx; i > idx; i--)
+                array[i + 1] = array[i];
+            array[idx] = c;
+            currentIdx++;
+        }
     }
     
-    // public void removeAt(int idx){}
+    public void remove(){
+        // do not decrease the size of the array if it is not big
+        // array[currentIdx] = '\0'; - not the approach to use
+    }
+    
+    // public void remove(int idx){}
     
     public char getAt(int idx){
         return array[idx];
