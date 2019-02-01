23bd61b128149a730270bc531be5e147a6bcf00f
MindaugasBernatavicius <darbas.mindaugas@gmail.com>
2019-02-01 08:17:12 +0200

    Adding the sorting function

--- java/algorithms/BubbleSortUDT.java
+++ java/algorithms/BubbleSortUDT.java
@@ -1,12 +1,34 @@
 public class BubbleSortUDT {
     public static void main(String args[]) {
-        Person[] a = { new Person("Jonas", "Jonaitis", 10),
+        Person[] a = { new Person("Jonas", "Jonaitis", 40),
                         new Person("Tomas", "Tomaitis", 20),
                         new Person("Maksas", "Maksaitis", 30) };
         
+        System.out.print("Before sorting: ");
         for(int i = 0; i < a.length; i++)
-            System.out.println(a[i].age);
+            System.out.print(a[i].age + " ");
+        System.out.println();
+            
+        bubbleSort(a);
+        
+        System.out.print("After sorting: ");
+        for(int i = 0; i < a.length; i++)
+            System.out.print(a[i].age + " ");
+        System.out.println();
     }
+    
+    public static void bubbleSort(Person arr[]){
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++){
+            for (int j = 0; j < n-i-1; j++){
+                if (arr[j].age > arr[j+1].age){
+                    Person temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    } 
 }
 
 class Person {
