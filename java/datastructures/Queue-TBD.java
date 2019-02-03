f100d4b694d01106875cc7a74f53ead2592108ee
MindaugasBernatavicius <darbas.mindaugas@gmail.com>
2019-02-03 14:10:23 +0200

    Update Queue-TBD.java

--- java/datastructures/Queue-TBD.java
+++ java/datastructures/Queue-TBD.java
@@ -1,6 +1,21 @@
 public class Queue {
     public static void main(String args[]) {
         QueueImpl q = new QueueImpl();
+        q.enqueue('a');
+        q.enqueue('b');
+        q.enqueue('c');
+        q.enqueue('d');
+        q.enqueue('e');
+        // q.print();
+        
+        while(!q.isEmpty())
+            System.out.print(q.dequeue());
+           
+        // TODO :: fix a bug, where the tailIdx
+        // does not get restored when we reach the 
+        // end of the queue
+        q.enqueue('c');
+        System.out.println(q.dequeue());
     }
 }
 
@@ -8,25 +23,37 @@ class QueueImpl {
     
     char[] queue;
     int headIdx;
+    int tailIdx;
     
     public QueueImpl(){
         queue = new char[5];
+        tailIdx = headIdx = queue.length - 1;
     }
     
     public void enqueue(char c){
-        // add resizing logic
-        queue[headIdx] = c;
-        headIdx++;
+        // add resizing logic if you want
+        queue[headIdx--] = c;
     }
+    
     public char dequeue(){
-        return queue[0];
+        return queue[tailIdx--];
     }
     
     public char tail(){
-        return queue[0];
+        return queue[tailIdx];
     }
     
     public char head(){
         return queue[headIdx];
+    }
+    
+    public boolean isEmpty(){
+        return (tailIdx == headIdx);
+    }
+    
+    public void print(){
+        for(int i = 0; i < queue.length; i++)
+            System.out.print(queue[i]);
+        System.out.println();
     }
 }
