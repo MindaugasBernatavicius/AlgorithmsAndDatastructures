4ce7d7368cfb70b7144042dd2f38c050253939d7
MindaugasBernatavicius <darbas.mindaugas@gmail.com>
2019-02-04 12:13:17 +0200

    Update BubbleSortUDT.java

--- java/algorithms/BubbleSortUDT.java
+++ java/algorithms/BubbleSortUDT.java
@@ -1,5 +1,22 @@
 public class BubbleSortUDT {
     public static void main(String args[]) {
+        
+        // 1st. part - array of objects
+        // Person mindaugas = new Person(10, "Mindaugas");
+        // Person petras = new Person(12, "Petras");
+        
+        // Person[] people = new Person[2];
+        // people[0] = mindaugas;
+        // people[1] = petras;
+        
+        // people[0].name = "XXX"; // same as mindaugas.name = "XXX";
+        
+        // for(int i = 0; i < people.length; i++)
+        //     System.out.print(people[i].name + " ");
+        // System.out.println();
+        
+        // System.out.println(mindaugas.name);
+        
         Person[] a = { new Person("Jonas", "Jonaitis", 40),
                         new Person("Tomas", "Tomaitis", 20),
                         new Person("Maksas", "Maksaitis", 30) };
