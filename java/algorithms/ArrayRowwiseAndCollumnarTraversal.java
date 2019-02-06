c6238f102c5bd58cc6c606c2c6ddd60c32315210
MindaugasBernatavicius <darbas.mindaugas@gmail.com>
2019-02-06 16:02:42 +0200

    Create ArrayRowwiseAndCollumnarTraversal.java

---
+++ java/algorithms/ArrayRowwiseAndCollumnarTraversal.java
@@ -0,0 +1,8 @@
+public class ArrayRowwiseAndCollumnarTraversal {
+  public static void main(String[] args){
+    // 0. Loop through increasing the size of matrix.
+    // 1. Row-wise traversal.
+    // 2. Collum-wise traversal (same array).
+    // 3. Print results
+  }
+}
