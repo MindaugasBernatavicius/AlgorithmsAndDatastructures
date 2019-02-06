ccdae77751b6b9aa8564d52ae8ca40fa136700c5
MindaugasBernatavicius <darbas.mindaugas@gmail.com>
2019-02-06 15:41:47 +0200

    Create ArrayTraversalSmallLarge.java

---
+++ java/algorithms/ArrayTraversalSmallLarge.java
@@ -0,0 +1,9 @@
+public class ArrayTraversalSmallLarge {
+  public status void main(String[] args){
+    // loop tests - each x100 tests use an increasingly bigger array elements
+    // int[][] arr = new[1000][100];
+    // int[][] arr = new[1000][1000];
+    // int[][] arr = new[1000][10000];
+    // ... or something similar
+  }
+}
