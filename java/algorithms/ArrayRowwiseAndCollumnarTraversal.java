85332750fddaa9ad265ab24f114b9f4127bb2c44
bernam <mindaugas@example.com>
2019-02-08 18:58:50 +0200

    Adding initial implementation of cache localization and overflow testing

--- java/algorithms/ArrayRowwiseAndCollumnarTraversal.java
+++ java/algorithms/ArrayRowwiseAndCollumnarTraversal.java
@@ -1,8 +1,63 @@
+package algorithms;
+
 public class ArrayRowwiseAndCollumnarTraversal {
   public static void main(String[] args){
+    int testCount = 1;
+    int initRowCount = 3;
+    int initCollumnCount = 3;
+    int multiplier = 2;
+
     // 0. Loop through increasing the size of matrix.
-    // 1. Row-wise traversal.
-    // 2. Collum-wise traversal (same array).
-    // 3. Print results
+    for(int i = 0; i < testCount; i++){
+      // 1. Define a new, bigger matrix on each loop
+      Integer[][] matrix = new Integer[initRowCount * multiplier][initCollumnCount * multiplier];
+
+      // 2. Fill the array and print it
+      for(int j = 0; j < matrix.length; j++){
+        for(int k = 0; k < matrix[j].length; k++){
+          matrix[j][k] = (int)(Math.random() * 99 + 1);
+        }
+      }
+      printMatrix(matrix);
+
+      // 3. Row-wise traversal.
+      increaseMatrixRowwise(matrix);
+      printMatrix(matrix);
+
+
+      // 4. Collum-wise traversal (same array).
+      increaseMatrixCollumnwise(matrix);
+      printMatrix(matrix);
+
+    }
+
+    // 4. Print results.
+
   }
+
+  private static <T> void printMatrix(T[][] matrix){
+    for(int j = 0; j < matrix.length; j++){
+      for(int k = 0; k < matrix[j].length; k++)
+        System.out.print(matrix[j][k] + " ");
+      System.out.println();
+    }
+    System.out.println();
+  }
+
+  private static void increaseMatrixRowwise(Integer[][] matrix) {
+    for (int j = 0; j < matrix.length; j++) {
+      for (int k = 0; k < matrix[0].length; k++) {
+        matrix[j][k] += 1;
+      }
+    }
+  }
+
+  private static void increaseMatrixCollumnwise(Integer[][] matrix) {
+    for(int j = 0; j < matrix[0].length; j++){
+      for(int k = 0; k < matrix.length; k++){
+        matrix[j][k] += 1;
+      }
+    }
+  }
+
 }
--- java/algorithms/ArrayTraversalSmall2Large.java
+++ java/algorithms/ArrayTraversalSmall2Large.java
@@ -1,5 +1,7 @@
+package algorithms;
+
 public class ArrayTraversalSmall2Large {
-  public status void main(String[] args){
+  public static void main(String[] args){
     // loop tests - each x100 tests use an increasingly bigger array elements
     // int[][] arr = new[1000][100];
     // int[][] arr = new[1000][1000];
