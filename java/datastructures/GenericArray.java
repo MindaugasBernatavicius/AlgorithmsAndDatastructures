2d1874c491c446ac33c043b839e0a288e72c1e31
MindaugasBernatavicius <darbas.mindaugas@gmail.com>
2019-02-11 13:44:15 +0200

    Create GenericArray.java

---
+++ java/datastructures/GenericArray.java
@@ -0,0 +1,51 @@
+class GenericArray<E> {
+
+	private final Object[] arr;
+	public final int length;
+
+	public Array(int length) {
+		// Creates a new Object array of specified length
+		arr = new Object[length];
+		this.length = length;
+	}
+
+	// Function to get Object present at index i in the array
+	E get(int i) {
+		@SuppressWarnings("unchecked")
+		final E e = (E)arr[i];
+		return e;
+	}
+
+	// Function to set a value e at index i in the array
+	void set(int i, E e) {
+		arr[i] = e;
+	}
+
+	@Override
+	public String toString() {
+		return Arrays.toString(arr);
+	}
+}
+
+
+	// Program to create a generic array in Java
+	public static void main(String[] args)
+	{
+		final int length = 5;
+
+		// create an Integer array of given length
+		GenericArray<Integer> intArray = new GenericArray(length);
+
+		for (int i = 0; i < length; i++)
+			intArray.set(i, i + 1);
+
+		System.out.println(intArray);
+
+		// create a String array of given length
+		GenericArray<String> strArray = new GenericArray(length);
+
+		for (int i = 0; i < length; i++)
+			strArray.set(i, String.valueOf((char)(i + 65)));
+
+		System.out.println(strArray);
+	}
